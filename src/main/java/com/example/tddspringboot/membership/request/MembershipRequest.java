b863a838a389d398e26869e836f91e67595fb32b
77kkyu <77kkyu@gmail.com>
2021-12-23 00:59:35 +0900

    feat MembershipConstants

---
+++ src/main/java/com/example/tddspringboot/membership/request/MembershipRequest.java
@@ -0,0 +1,25 @@
+package com.example.tddspringboot.membership.request;
+
+import com.example.tddspringboot.membership.domain.MembershipType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+@NoArgsConstructor(force = true)
+public class MembershipRequest {
+
+    @NotNull
+    @Min(0)
+    private final Integer point;
+
+    @NotNull
+    private final MembershipType membershipType;
+
+}
