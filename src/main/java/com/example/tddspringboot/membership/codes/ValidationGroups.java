c9b68510d11a52192ed85bea0369821fb6a59703
77kkyu <77kkyu@gmail.com>
2021-12-26 00:13:56 +0900

    feat ValidationGroups

---
+++ src/main/java/com/example/tddspringboot/membership/codes/ValidationGroups.java
@@ -0,0 +1,11 @@
+package com.example.tddspringboot.membership.codes;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class ValidationGroups {
+
+    public interface MembershipAddMarker { }
+    public interface MembershipAccumulateMarker { }
+
+}
