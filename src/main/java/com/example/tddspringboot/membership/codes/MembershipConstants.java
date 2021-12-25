de2dd6616cd0afae8c9068d69b136294f5cd4393
77kkyu <77kkyu@gmail.com>
2021-12-25 18:14:11 +0900

    feat MembershipConstants

---
+++ src/main/java/com/example/tddspringboot/membership/codes/MembershipConstants.java
@@ -0,0 +1,11 @@
+package com.example.tddspringboot.membership.codes;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class MembershipConstants {
+
+    public final static String USER_ID_HEADER = "X-USER-ID";
+
+}
