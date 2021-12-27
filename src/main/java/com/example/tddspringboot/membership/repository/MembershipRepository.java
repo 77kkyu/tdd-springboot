4e6cd085be299bab9b15179fdb0fce56f7697705
77kkyu <77kkyu@gmail.com>
2021-12-27 21:30:25 +0900

    feat MembershipRepository

--- src/main/java/com/example/tddspringboot/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/tddspringboot/membership/repository/MembershipRepository.java
@@ -2,10 +2,15 @@ package com.example.tddspringboot.membership.repository;
 
 import com.example.tddspringboot.membership.domain.Membership;
 import com.example.tddspringboot.membership.domain.MembershipType;
+import com.example.tddspringboot.membership.model.MembershipDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
 
     Membership findByUserIdAndMembershipType(final String userId, final MembershipType membershipType);
+
+    List<Membership> findAllByUserId(final String userId);
 
 }
